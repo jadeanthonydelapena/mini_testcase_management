157d9a0b10518e485ca0fb7eb75c9d93a9a540b0
unknown <jadeanthonydelapena@gmail.com>
2025-06-30 18:20:45 +0800

    Add more views and configured URL

--- mini_project/testcases/urls.py
+++ mini_project/testcases/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:test_case_id>/", views.display_test_case_detail, name="detail"),
+    path("<int:test_case_id>/steps/", views.display_test_case_steps, name="steps"),
 ]
--- mini_project/testcases/views.py
+++ mini_project/testcases/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Running the testcases app.")
+
+def display_test_case_detail(request, test_case_id):
+    return HttpResponse(f"You are looking at the details of Test Case {test_case_id}.")
+
+def display_test_case_steps(request, test_case_id):
+    return HttpResponse(f"You are looking at the test steps of Test Case {test_case_id}.")
