13ac7b486a537e992206f23008529c14c657754e
Jade de la Peña <jade.delapena@easesolutions.com>
2025-07-09 20:45:11 +0800

    Dynamic Test Steps

--- mini_project/testcases/urls.py
+++ mini_project/testcases/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("testcases/<int:test_case_id>/", views.display_test_case_detail, name="detail"),
     path('testcases/add-test-case/', views.add_new_test_case, name="add-new-test-case"),
+    # path('testcases/add-test-case/', views.dummy, name="add-new-test-case"),
     path('testcases/add-test-case/success-add-test-case/', views.success_add_test_case, name="success-add-test-case"),
 ]
--- mini_project/testcases/views.py
+++ mini_project/testcases/views.py
@@ -17,26 +17,34 @@ def display_test_case_detail(request, test_case_id):
     test_case_steps = test_case.teststep_set.all()
 
     template = loader.get_template('testcases/test_case_detail.html')
-    context = {'test_case' : test_case, 'test_case_steps' : test_case_steps}
+    context = { 'test_case_id': f"{test_case.id:03}", 'test_case' : test_case, 'test_case_steps' : test_case_steps}
     return HttpResponse(template.render(context, request))
 
 def add_new_test_case(request):
     if request.method == "POST":
-        form = NewTestCaseForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            t = TestCase(title=data['new_title'], owner="Jade De la Pena", date_created=timezone.now())
-            t.save()
+        actions = []
+        results = []
+        title = None
 
-            t.teststep_set.create(action=data['new_ts_action'], expected_result=data['new_ts_result'])
-            
-            return HttpResponseRedirect('success-add-test-case/')
+        for key, value in request.POST.items():
+            if 'title' in key:
+                title = value
+            elif key.startswith('step_action'):
+                actions.append(value)
+            elif key.startswith('step_result'):
+                results.append(value)
+
+        t = TestCase(title=title, owner="Jade De la Pena", date_created=timezone.now())
+        t.save()
+
+        for i in range(len(actions)):
+            t.teststep_set.create(action=actions[i], expected_result=results[i])
+
+        return HttpResponseRedirect('success-add-test-case/')
+    
     else:
-        form = NewTestCaseForm()
-
-    template = loader.get_template('testcases/add_test_case.html')
-    context = {"form": form}
-    return HttpResponse(template.render(context, request))
+        template = loader.get_template('testcases/add_test_case.html')
+        return HttpResponse(template.render({}, request))
 
 
 def success_add_test_case(request):
